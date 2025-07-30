adf8cf5f15e5230f1acd9569a75c22e7a5792d57
Atharva Gore <atharvagore033@gmail.com>
2025-07-30 11:37:41 +0530

    Create syllabus.js

---
+++ syllabus.js
@@ -0,0 +1,12 @@
+{
+  "computer": {
+    "sem5": "assets/syllabus/comp_sem5.pdf",
+    "sem4": "assets/syllabus/comp_sem4.pdf"
+  },
+  "mechanical": {
+    "sem5": "assets/syllabus/mech_sem5.pdf"
+  },
+  "civil": {
+    "sem5": "assets/syllabus/civil_sem5.pdf"
+  }
+}
